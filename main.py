7a9b7c7bd6946d8255798e3ba69f8bbc7ac9d33d
Canal-dodo8 <74329135+Matheus-Olifrans@users.noreply.github.com>
2021-12-31 14:21:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+class Numeros:
+  def __init__(self, lotery):
+    self.numbers = set(random.sample(range(1,61),6))
+    self.acertado = self.numbers.intersection(lotery)
+print('escolha os numeros sem repetir')
+loteria = {int(input(f'digite o {a}° numero: ')) for a in range(1,7)}
+
+Matheus = Numeros(loteria)
+Samuel = Numeros(loteria)
+print(f'numeors escolhidos para Matheus são "{Matheus.numbers}"\n')
+print(f'numeors escolhidos para Samuel são "{Samuel.numbers}"\n')
+print(f'numeros corretos de Matheus foram "{Matheus.acertado}" acertando {len(Matheus.acertado)}\n')
+print(f'numeros corretos de Samuel foram "{Samuel.acertado}" acertando {len(Samuel.acertado)}')
